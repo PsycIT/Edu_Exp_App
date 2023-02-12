b576fdfb92b0906d1ad8de06ace183dedc38a619
PsycIT <dlsxor48@naver.com>
2023-02-13 03:39:36 +0900

    completed main & test page

---
+++ test_page.py
@@ -0,0 +1,72 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+from PIL import Image
+
+
+form_class = uic.loadUiType("ui/test_page_window.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.fontSize = 10
+
+        self.teAns = 0
+
+        self.name = ""
+        self.birth = ""
+        self.expCnt = 0
+
+        # 문제지
+        # basewidth = 600
+        # self.setResizeImg(basewidth)
+
+        questPixmap = QPixmap("imgs/questions/2_resize.jpg")
+        self.testLabel.setPixmap(questPixmap)
+        self.testLabel.resize(questPixmap.width(), questPixmap.height())
+
+        # 선택지
+        self.ansRBtn1.clicked.connect(self.radioBtn_clicked)
+        self.ansRBtn2.clicked.connect(self.radioBtn_clicked)
+        self.ansRBtn3.clicked.connect(self.radioBtn_clicked)
+        self.ansRBtn4.clicked.connect(self.radioBtn_clicked)
+        self.ansRBtn5.clicked.connect(self.radioBtn_clicked)
+
+        self.teSubmitBtn.clicked.connect(self.teSubmitBtn_clicked)
+
+    def radioBtn_clicked(self):
+        if self.ansRBtn1.isChecked(): self.teAns = 1
+        elif self.ansRBtn2.isChecked(): self.teAns = 2
+        elif self.ansRBtn3.isChecked(): self.teAns = 3
+        elif self.ansRBtn4.isChecked(): self.teAns = 4
+        elif self.ansRBtn5.isChecked(): self.teAns = 5
+
+        print("The selected answer is ", self.teAns)
+
+    def teSubmitBtn_clicked(self):
+        self.name = "it"
+        self.birth = "1114"
+        self.expCnt = "1회차"
+
+        print(self.name, self.birth, self.expCnt)
+
+        QMessageBox.about(self, '선택정답', str(self.teAns)+'번')
+
+
+    # img 비율 유지하여 resizing & save
+    def setResizeImg(self, basewidth=600):
+        img = Image.open('imgs/questions/5.jpg')
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img_resize = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        img_resize.save('imgs/questions/5_resize.jpg')
+
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
